263c999db3705b7bb7b3711f84902aadff014c6a
ramimhossain12 <ramimhossain71.info@gmail.com>
2026-09-19 03:13:11 +0600

    Initial Commit

---
+++ Food Explorer App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Food Explorer App
+//
+//  Created by Ramim Hossain on 19/09/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Food Explorer App/Food_Explorer_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_Explorer_AppApp.swift
+//  Food Explorer App
+//
+//  Created by Ramim Hossain on 19/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct Food_Explorer_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
